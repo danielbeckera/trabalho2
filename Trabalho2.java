85b0874567a6b6fd98ad700e5f8d75588e3a0cf9
Daniel Becker Alves <danielbeckeralves@gmail.com>
2016-05-24 19:26:10 -0300

    main

---
+++ Trabalho2.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+public class Trabalho2 {
+	// Abstract data
+	static class Art {	
+		int compartimento;
+		int numeroVezes;
+		String music;	
+		String artist;		
+	}
+	// Questão 1) Procedimento numero de vezes que a música foi tocada
+	public static void numeroVezesTocada() {	
+		for(int i = 0; i<8; i++) {
+			
+			
+		}	
+}	
+	// Main 
+	public static void main(String[] args){
+	// Questão 1
+		numeroVezesTocada();
+		
+	}
+}
